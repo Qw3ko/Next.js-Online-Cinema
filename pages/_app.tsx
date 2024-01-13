933381e6ba88624f18924c3a8c726c3f5423e395
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-01-13 15:47:15 +0500

    updated main files

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app'
+
+import '../app/assets/styles/globals.scss'
+import MainProvider from '../app/providers/MainProvider'
+
+type TypeAppProps = AppProps
+
+const MyApp = ({ Component, pageProps }: TypeAppProps) => {
+	return (
+		<MainProvider>
+			<Component {...pageProps} />
+		</MainProvider>
+	)
+}
+
+export default MyApp
