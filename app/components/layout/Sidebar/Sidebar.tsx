f6b10ba5c8d291e67fb2f65452111f0241844df6
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-01-19 12:02:01 +0500

    updated sidebar component

--- app/components/layout/Sidebar/Sidebar.tsx
+++ app/components/layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
+
+import MoviesContainer from './MoviesContainer/MoviesContainer'
+import Search from './Search/Search'
 import styles from './Sidebar.module.scss'
 
-const Sidebar:FC = () => {
+const Sidebar: FC = () => {
 	return (
-		<div>Sidebar</div>
+		<div className={styles.sidebar}>
+			<Search />
+			<MoviesContainer />
+		</div>
 	)
 }
 
