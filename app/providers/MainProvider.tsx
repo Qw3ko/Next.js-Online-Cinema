f95c1b783a891e60ecce7fee8e8655d376bc23bc
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-01-23 23:02:44 +0500

    updated main provider

--- app/providers/MainProvider.tsx
+++ app/providers/MainProvider.tsx
@@ -6,6 +6,8 @@ import Layout from '@/components/layout/Layout'
 
 import { store } from '@/store/store'
 
+import AuthProvider from './AuthProvider/AuthProvider'
+import { TypeComponentAuthFields } from './AuthProvider/auth.types'
 import HeadProvider from './HeadProvider/HeadProvider'
 import ReduxToast from './ReduxToast'
 
@@ -17,13 +19,15 @@ const queryClient = new QueryClient({
 	},
 })
 
-const MainProvider: FC = ({ children }) => {
+const MainProvider: FC<TypeComponentAuthFields> = ({ children, Component }) => {
 	return (
 		<HeadProvider>
 			<Provider store={store}>
 				<QueryClientProvider client={queryClient}>
 					<ReduxToast />
-					<Layout>{children}</Layout>
+					<AuthProvider Component={Component}>
+						<Layout>{children}</Layout>
+					</AuthProvider>
 				</QueryClientProvider>
 			</Provider>
 		</HeadProvider>
