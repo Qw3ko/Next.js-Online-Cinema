525464b2d5779701b01fe29d398790703d2dd143
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-01-28 21:15:58 +0500

    added movie list component

---
+++ app/components/screens/admin/movies/useMovies.ts
@@ -0,0 +1,87 @@
+import { getAdminUrl } from 'config/url.config'
+import { ChangeEvent, useMemo, useState } from 'react'
+import { useMutation, useQuery } from 'react-query'
+import { toastr } from 'react-redux-toastr'
+
+import { ITableItem } from '@/components/ui/admin-table/AdminTable/admin-table.interface'
+
+import { useDebounce } from '@/hooks/useDebounce'
+
+import { MovieService } from '@/services/movie.service'
+
+import { getGenresList } from '@/utils/movie/getGenresList'
+import { toastError } from '@/utils/toast-error'
+import { useRouter } from 'next/router'
+
+export const useMovies = () => {
+	const [searchTerm, setSearchTerm] = useState('')
+	const debouncedSearch = useDebounce(searchTerm, 500)
+
+	const queryData = useQuery(
+		['movie list', debouncedSearch],
+		() => MovieService.getAll(debouncedSearch),
+		{
+			select: ({ data }) =>
+				data.map(
+					(movie): ITableItem => ({
+						_id: movie._id,
+						editUrl: getAdminUrl(`movie/edit/${movie._id}`),
+						items: [
+							movie.title,
+							getGenresList(movie.genres),
+							String(movie.rating),
+						],
+					})
+				),
+
+			onError: (error) => {
+				toastError(error, 'Movie list')
+			},
+		}
+	)
+
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchTerm(e.target.value)
+	}
+
+	const { push } = useRouter()
+
+	const { mutateAsync: createAsync } = useMutation(
+		'create genre',
+		() => MovieService.create(),
+		{
+			onError: (error) => {
+				toastError(error, 'Create movieMovieService')
+			},
+			onSuccess: ({ data: _id }) => {
+				toastr.success('Create movieMovieService', 'Create was successful')
+				push(getAdminUrl(`movieMovieService/edit/${_id}`))
+			},
+		}
+	)
+
+	const { mutateAsync: deleteAsync } = useMutation(
+		'delete movie',
+		(movieId: string) => MovieService.delete(movieId),
+		{
+			onError: (error) => {
+				toastError(error, 'Delete movie')
+			},
+			onSuccess: () => {
+				toastr.success('Delete movie', 'Delete was successful')
+				queryData.refetch()
+			},
+		}
+	)
+
+	return useMemo(
+		() => ({
+			searchTerm,
+			createAsync,
+			handleSearch,
+			...queryData,
+			deleteAsync,
+		}),
+		[searchTerm, queryData, deleteAsync, createAsync]
+	)
+}
