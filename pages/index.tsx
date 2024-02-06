6224a3621450db989b9d23929299695a2b5f5b05
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-02-06 14:11:13 +0500

    added pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,73 @@
-import type { NextPage } from 'next'
+import { getActorUrl, getMovieUrl } from 'config/url.config'
+import type { GetStaticProps, NextPage } from 'next'
 
 import Home from '@/screens/home/Home'
 
-const HomePage: NextPage = (props) => {
-	return <Home {...props} />
+import { IHome } from '@/components/screens/home/Home.interface'
+import { IGalleryItem } from '@/components/ui/gallery/gallery.interface'
+import { ISlide } from '@/components/ui/slider/slider.interface'
+
+import { ActorService } from '@/services/actor.service'
+import { MovieService } from '@/services/movie.service'
+
+import { getGenresList } from '@/utils/movie/getGenresList'
+
+const HomePage: NextPage<IHome> = ({ slides, actors, trendingMovies }) => {
+	return (
+		<Home slides={slides} actors={actors} trendingMovies={trendingMovies} />
+	)
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	try {
+		const { data: movies } = await MovieService.getAll()
+
+		const slides: ISlide[] = movies.slice(0, 3).map((m) => ({
+			_id: m._id,
+			link: getMovieUrl(m.slug),
+			subTitle: getGenresList(m.genres),
+			title: m.title,
+			bigPoster: m.bigPoster,
+		}))
+
+		const { data: dataActors } = await ActorService.getAll()
+
+		const actors: IGalleryItem[] = dataActors.slice(0, 7).map((a) => ({
+			name: a.name,
+			posterPath: a.photo,
+			link: getActorUrl(a.slug),
+			content: {
+				title: a.name,
+				subTitle: `+${a.countMovies} movies`,
+			},
+		}))
+
+		const dataTrendingMovies = await MovieService.getMostPopularMovies()
+
+		const trendingMovies: IGalleryItem[] = dataTrendingMovies
+			.slice(0, 7)
+			.map((m) => ({
+				name: m.title,
+				posterPath: m.poster,
+				link: getMovieUrl(m.slug),
+			}))
+
+		return {
+			props: {
+				slides,
+				actors,
+				trendingMovies,
+			} as IHome,
+		}
+	} catch (error) {
+		return {
+			props: {
+				slides: [],
+				actors: [],
+				trendingMovies: [],
+			},
+		}
+	}
 }
 
 export default HomePage
--- pages/trending.tsx
+++ pages/trending.tsx
@@ -1,7 +1,35 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 
-const TrendingPage: NextPage = () => {
-	return <div>TrendingPage</div>
+import Catalog from '@/components/ui/catalog-movies/Catalog'
+
+import { IMovie } from '@/shared/types/movie.types'
+
+import { MovieService } from '@/services/movie.service'
+
+const TrendingPage: NextPage<{ movies: IMovie[] }> = ({ movies }) => {
+	return (
+		<Catalog
+			movies={movies || []}
+			title="Fresh movies"
+			description="Trending movies in excellent quality: legal, safe, without ads"
+		/>
+	)
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	try {
+		const movies = await MovieService.getMostPopularMovies()
+
+		return {
+			props: {
+				movies,
+			},
+		}
+	} catch (error) {
+		return {
+			notFound: true,
+		}
+	}
 }
 
 export default TrendingPage
