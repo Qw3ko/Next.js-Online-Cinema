b9ae96e3bf03174fdc862b3dee76ff044ba121f2
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-02-06 14:10:10 +0500

    updated actor list component

--- app/components/screens/admin/actors/ActorList.tsx
+++ app/components/screens/admin/actors/ActorList.tsx
@@ -10,13 +10,24 @@ import { Meta } from '@/utils/meta'
 import { useActors } from './useActor'
 
 const ActorList: FC = () => {
-	const { handleSearch, isLoading, searchTerm, data, deleteAsync } = useActors()
+	const {
+		handleSearch,
+		isLoading,
+		searchTerm,
+		data,
+		deleteAsync,
+		createAsync,
+	} = useActors()
 
 	return (
 		<Meta title="Actors">
 			<AdminNavigation />
 			<Heading title="Actors" />
-			<AdminHeader handleSearch={handleSearch} searchTerm={searchTerm} />
+			<AdminHeader
+				handleSearch={handleSearch}
+				searchTerm={searchTerm}
+				onClick={createAsync}
+			/>
 			<AdminTable
 				isLoading={isLoading}
 				removeHandler={deleteAsync}
