854344762d476a319d45c52bcf31216b6b8b97e5
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-02-06 14:09:37 +0500

    updated services

--- app/services/actor.service.ts
+++ app/services/actor.service.ts
@@ -1,5 +1,8 @@
 import axios, { axiosClassic } from 'api/interceptors'
 import { getActorsUrl } from 'config/api.config'
+import { getActorUrl } from 'config/url.config'
+
+import { IActorEditInput } from '@/components/screens/admin/actor/actor-edit.interface'
 
 import { IActor } from '@/shared/types/movie.types'
 
@@ -14,8 +17,20 @@ export const ActorService = {
 		})
 	},
 
+	async getBySlug(slug: string) {
+		return axiosClassic.get<IActor>(getActorsUrl(`/by-slug/${slug}`))
+	},
+
+	async getById(_id: string) {
+		return axios.get<IActorEditInput>(getActorsUrl(`/${_id}`))
+	},
+
 	async create() {
 		return axios.post<string>(getActorsUrl(`/`))
+	},
+
+	async update(_id: string, data: IActorEditInput) {
+		return axios.put<string>(getActorsUrl(`/${_id}`), data)
 	},
 
 	async delete(_id: string) {
