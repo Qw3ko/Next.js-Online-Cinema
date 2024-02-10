5fa2570aa45f2c3f5d1028f76b330e749ea4e0c0
Qw3ko <bannikov.arseniyy@rambler.ru>
2024-02-10 16:17:59 +0500

    updated catalog component

--- app/components/ui/catalog-movies/Catalog.tsx
+++ app/components/ui/catalog-movies/Catalog.tsx
@@ -1,11 +1,11 @@
 import { getMovieUrl } from 'config/url.config'
 import { FC } from 'react'
 
-import { Meta } from '@/utils/meta'
+import GalleryItem from '@/components/ui/gallery/GalleryItem'
+import Description from '@/components/ui/heading/Description'
+import Heading from '@/components/ui/heading/Heading'
 
-import GalleryItem from '../gallery/GalleryItem'
-import Description from '../heading/Description'
-import Heading from '../heading/Heading'
+import { Meta } from '@/utils/meta'
 
 import styles from './Catalog.module.scss'
 import { ICatalog } from './catalog.interface'
@@ -14,28 +14,34 @@ const Catalog: FC<ICatalog> = ({ title, description, movies }) => {
 	return (
 		<Meta title={title} description={description}>
 			<Heading title={title} className={styles.heading} />
-
 			{description && (
 				<Description text={description} className={styles.description} />
 			)}
 
 			<section className={styles.movies}>
-				{movies.map((movie) => (
-					<GalleryItem
-						key={movie._id}
-						item={{
-							name: movie.title,
-							link: getMovieUrl(movie.slug),
-							posterPath: movie.bigPoster,
-							content: {
-								title: movie.title,
-							},
-						}}
-						variant="horizontal"
-					/>
-				))}
+				{Array.isArray(movies)
+					? movies.map((movie) => (
+							<GalleryItem
+								key={movie._id}
+								variant="horizontal"
+								item={{
+									name: movie.title,
+									posterPath: movie.bigPoster,
+									url: getMovieUrl(movie.slug),
+									content: {
+										title: movie.title,
+									},
+								}}
+							/>
+					  ))
+					: null}
 			</section>
+
+			{/* <div className="text-center">
+				<button className={styles.button}>Load more</button>
+			</div> */}
 		</Meta>
 	)
 }
+
 export default Catalog
